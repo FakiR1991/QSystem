8eda5cce3b6a5b8ada163f73a3af5c5ad5518798
zaikov <zaikov.andrey@gmail.com>
2026-06-24 11:53:22 +0300

    commit

--- src/ru/apertum/qsystem/common/model/QCustomer.java
+++ src/ru/apertum/qsystem/common/model/QCustomer.java
@@ -40,8 +40,6 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.ServiceLoader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.Transient;
